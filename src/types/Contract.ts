f29560cabdd0048a8821e4fd79f9a6a3d1393995
TinyBlondMan <nicolas.s.humbert@gmail.com>
2023-08-01 13:54:50 +0200

    Added Contract type;

---
+++ src/types/Contract.ts
@@ -0,0 +1,17 @@
+export type Contract = {
+  proposition_num: string;
+  code_product: string;
+  client_code: string;
+  payeur_code: string;
+  name_client: string;
+  name_payeur: string;
+  creation_date: string;
+  effect_date: string;
+  expiry_date: string;
+  code_agent: string;
+  name_redac: string;
+  officeid_bur_dir: string;
+  contract_statusid_c_status: string;
+  contracts_typeid_contract_type: string;
+  agentsid_agent: string;
+};
