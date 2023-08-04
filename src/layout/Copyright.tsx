34173ed374c8ac794d21599c14d29c7fed1aad4f
TinyBlondMan <nicolas.s.humbert@gmail.com>
2023-08-04 20:03:20 +0200

    Added copyright;

---
+++ src/layout/Copyright.tsx
@@ -0,0 +1,11 @@
+import "../styles/copyright.scss";
+
+const Copyright = () => {
+  return (
+    <footer className="footer">
+      <small>&copy; 2023 - White Carrot Inc.</small>{" "}
+    </footer>
+  );
+};
+
+export default Copyright;
