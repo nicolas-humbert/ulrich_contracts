f4a1c2884181cc14a29b5fe05215fd5d70333556
TinyBlondMan <nicolas.s.humbert@gmail.com>
2023-08-13 11:35:43 +0200

    Added styling and lorem content for About page;

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,79 @@
+import PageTitle from "../components/PageTitle";
+import "../styles/about-.scss";
+
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div id="About">
+      <PageTitle text="À propos" />
+
+      <main>
+        <h2>Utilisation</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          delectus ducimus fuga, fugit, placeat exercitationem impedit non
+          adipisci, beatae neque iure! Enim fugit error illum sunt et doloremque
+          tenetur saepe dolorum repudiandae dolor dolore ipsum numquam nesciunt
+          reprehenderit excepturi, ratione aliquam suscipit itaque nostrum atque
+          natus nisi hic non perferendis. Corrupti, odio quis quia vitae omnis
+          fuga harum nostrum ea numquam, nobis quisquam, animi repellendus unde
+          dolor veniam minima. Reprehenderit nihil repellat quo qui ullam eos
+          fugit voluptas at doloribus, fugiat, odio earum, laborum delectus aut
+          nostrum itaque quam iste tempore voluptatem excepturi officiis. Non
+          rem beatae laborum facere expedita!
+        </p>
+
+        <h2>Procédures</h2>
+        <p>
+          Nulla molestias corporis sequi laudantium repellendus, quo ex, maxime
+          dolor quisquam saepe ipsam aliquam repudiandae? Voluptatem cupiditate
+          eius vero iste at velit maxime saepe eaque ipsa reprehenderit facilis
+          excepturi laboriosam hic itaque voluptates pariatur ut ea architecto
+          earum adipisci, aperiam, totam ab. Vero sunt et facere eius similique.
+          Odio eaque fugit vero? Accusantium asperiores quo aliquam laboriosam
+          sed laudantium facilis mollitia velit ut. Quas quis temporibus
+          cupiditate beatae sequi. <br />
+          Expedita illum reprehenderit quibusdam rem aperiam id autem sed facere
+          quisquam nisi, distinctio tempora, doloremque natus pariatur, soluta
+          quia modi! Suscipit rem natus est ad? In blanditiis, velit dolorem
+          nihil, laudantium, facilis inventore ipsa ratione eaque ut laborum
+          consectetur necessitatibus voluptas provident natus harum qui illum?
+          Pariatur necessitatibus in ex, et ducimus minus, nesciunt illo tempora
+          harum iste vero accusantium maxime magni consectetur earum? Vel et
+          velit adipisci tenetur quas iusto praesentium ducimus facere delectus
+          inventore neque, nemo doloribus? Veniam, tenetur amet magni eligendi
+          est maxime cumque soluta reiciendis dolorum autem, iure ut quas labore
+          minima exercitationem! <br />
+          Consequatur, porro magni. Quae aliquid illo sapiente qui illum cumque
+          reiciendis ducimus nesciunt deserunt quo ipsam, eum voluptas ratione
+          natus commodi accusamus labore animi. Suscipit inventore repellat iste
+          nisi commodi ab repellendus omnis iure quo, eveniet voluptates earum
+          similique ratione deleniti blanditiis, excepturi voluptate repudiandae
+          placeat sapiente quidem impedit saepe! Voluptas inventore quos,
+          accusantium itaque iusto assumenda beatae magni vero veniam aliquam
+          ipsa sed, nulla unde numquam impedit et, neque deserunt quas. Corporis
+          perferendis numquam consectetur architecto quidem ab aspernatur. Odit
+          molestias tenetur magni placeat, dolore cupiditate quos consequuntur,
+          sint molestiae maxime perspiciatis cumque quasi hic, repudiandae eum
+          quo necessitatibus deleniti laborum. Accusantium nesciunt ratione
+          sequi voluptate esse commodi culpa voluptas recusandae consequuntur
+          molestias, quo laborum accusamus. Atque, eligendi voluptates.
+        </p>
+
+        <h2>Politique de confidentialité</h2>
+        <p>
+          Est ipsum saepe ullam inventore illo quas, dolores nobis, tempora
+          nesciunt commodi, provident sint. Laudantium ducimus voluptate aperiam
+          sunt praesentium est earum doloribus dignissimos ipsum unde cupiditate
+          voluptates saepe, inventore reprehenderit quasi, tempore laboriosam.
+          Possimus eaque id tempore velit quasi voluptas cumque soluta. Aut
+          dolorum culpa perferendis, pariatur maiores cupiditate, quod at
+          perspiciatis odit temporibus ipsum exercitationem eaque et aliquam,
+          similique eius. Impedit nostrum doloribus recusandae ratione quis
+          ipsum perspiciatis repellendus facilis, repudiandae adipisci.
+        </p>
+      </main>
+    </div>
+  );
 };
 
 export default About;
